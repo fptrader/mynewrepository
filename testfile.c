7478d9186f41954e1377a66af21af3329d7a5e71
Alan <alangoldsbrough@hotmail.com>
2020-08-16 13:49:51 +0200

    commit merged branch

--- testfile.c
+++ testfile.c
@@ -13,5 +13,24 @@ this line is only on branch
 
 A line added on the master
 
-change this line alredy changed on mast er shoul create conflict
+change same line to crate conflict
 
+A line wch should be on the branch
+
+add another line
+
+one more
+
+add a line on my branch
+add another line on my branch
+
+the black dots are increasing - and the black lline showing my commits on my branch
+change this line alredy changed on mast er shoul create conflict
+Add aline on moniras branch
+
+Add a line on branch montira_21_06 etc
+
+
+Can I merge this line
+
+I must request reveiw and should not be able to merge cos I protected the master branch form within github
